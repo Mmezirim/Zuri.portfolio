7af924ec26db4f8f29ecbce2b4f6932f94105a9e
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2022-06-18 19:26:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http');  
+const url = require('url');  
+const server = http.createServer(function(request, response) {  
+    const path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/':  
+            response.writeHead(200, {  
+                'Content-Type': 'text/plain'  
+            });  
+            response.write("This is Test Message.");  
+            response.end();  
+            break;  
+        case '/index.html':  
+            fs.readFile(__dirname + "/index.html", function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break; 
+          //  Yet to fully grasp but just incase adding another file 
+        case 'home.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+      }
+});  
+server.listen(8082, function(){
+      console.log('welcome to my server')
+});  
