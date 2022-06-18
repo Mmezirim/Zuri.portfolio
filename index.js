1f101f732c801f4b608ab802093a9273064d3dcf
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2022-06-18 14:17:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
 
-
-app.get('home.html', function(req, res){
+app.get('/render.html', function(req, res){
        res.sendFile(__dirname + "/index.html")
 })
+//till the concept is established, just incase.
+app.get('/render.html', function(req, res){
+       res.sendFile(__dirname + "/home.html")
+})
+app.get('/render.html', function(req, res){
+       res.sendFile(__dirname + "/about.html")
+})
+app.get('/render.html', function(req, res){
+       res.sendFile(__dirname + "/contact.html")
+})
 
-
-app.listen(3000, function(){
+app.listen(5001, function(){
       console.log('server is up and running')
 })
